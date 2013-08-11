d1607326988ac4e1213b458098aa5ec413de338c
Michael Brooks <mjbrooks@uw.edu>
2013-08-10 17:19:36 -0700

    update twitter button

--- web/index.php
+++ web/index.php
@@ -83,8 +83,9 @@ if (isset($_GET['id'])) {
                 <a href="https://twitter.com/share" 
                     class="twitter-share-button" 
                     data-url="<?php echo $alot_url; ?>" 
-                    data-via="seethatalot" 
-                    data-hashtags="alot">Tweet</a>
+                    data-via="seethatalot"
+                    data-text="I see #alot of <?php echo $word ?>!"
+                    >Tweet</a>
                 <!-- Twitter JS -->
                 <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
             
